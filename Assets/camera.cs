cf39829013b6f8db024be73686f9040cdc21706e
guiomarcelino <53095188+guiomarcelino@users.noreply.github.com>
2021-04-21 12:59:16 -0300

    ajuste camera

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -1,26 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class camera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+    public float deltaY;
+
+    public GameObject player;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        smoothTimeY = 0.2f;
+        smoothTimeX = 0.2f;
+        deltaY = 0.4f;
     }
-    //
 
-    //fazer um delay na camera. 
-    /*private void LateUpdate()
+    void FixedUpdate()
     {
-      transform.Translate(0, Time.deltaTime, 0);
-    }*/
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + deltaY, ref velocity.y, smoothTimeY);
+        if (player.transform.position.x < -13.22f)
+        {
+            transform.position = new Vector3(-13.22f, -0.01650012f, transform.position.z);
+        }else if (player.transform.position.x > 37.74f)
+        {
+            transform.position = new Vector3(37.74f, -0.01650012f, transform.position.z);
+        }else 
+        {
+            transform.position = new Vector3(posX, -0.01650012f, transform.position.z);
+        }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-}
+    }
 }
--- Assets/playersControler.cs
+++ Assets/playersControler.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class playersControler : MonoBehaviour
 {
-    public float    speedForce = 15.0f;
-    public float    jumpForce = 8.0f;
-    public Vector3  offset = Vector2.zero;
-    public float    raidios = 10.0f;
-    public Vector2  bonecoPos;
-    public Vector2  _moviment = Vector2.zero;
+    public float speedForce = 15.0f;
+    public float jumpForce = 8.0f;
+    public Vector3 offset = Vector2.zero;
+    public float raidios = 10.0f;
+    public Vector2 bonecoPos;
+    public Vector2 _moviment = Vector2.zero;
     public Animator animator;
-    public bool     taNoChao = true;
-    public bool     taNaAgua;
+    public bool taNoChao = true;
+    public bool taNaAgua;
     public Transform detectaChao;
     public Transform detectaAgua;
     public LayerMask oQueEhChao;
     public LayerMask oQueEhAgua;
     public Rigidbody2D rb;
-    public int      movespeed;
-    public int      pulosExtras = 1;
+    public int movespeed;
+    public int pulosExtras = 1;
+    public int pisca2;
 
-    private Vector2     _input = Vector2.zero;
-    private Vector2     _direction = Vector2.zero;
+    private Vector2 _input = Vector2.zero;
+    private Vector2 _direction = Vector2.zero;
     private Rigidbody2D _body = null;
     private SpriteRenderer _renderer = null;
-    private float       direction;
-    private Vector3     facingRight;
-    private Vector3     facingLeft;
+    private float direction;
+    private Vector3 facingRight;
+    private Vector3 facingLeft;
+    private int pisca;
 
     [SerializeField]
     private bool isDeath = false;
@@ -38,7 +40,7 @@ public class playersControler : MonoBehaviour
         _body = GetComponent<Rigidbody2D>();
         _renderer = GetComponent<SpriteRenderer>();
     }
- 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,18 +58,7 @@ public class playersControler : MonoBehaviour
         {
             direction = Input.GetAxis("Horizontal");
             rb.velocity = new Vector2(direction * movespeed, rb.velocity.y);
-            /*
-            if (direction > 0)
-            {
-                //olhando para a direita
-                transform.localScale = facingRight;
-            }
-            if (direction < 0)
-            {
-                //olhando para a esquerda
-                transform.localScale = facingLeft;
-            }
-            */
+
             if (Input.GetButtonDown("Jump") && taNoChao == true)
             {
                 rb.velocity = Vector2.up * jumpForce;
@@ -96,6 +87,7 @@ public class playersControler : MonoBehaviour
             {
                 Debug.Log("Morreu");
                 StartCoroutine(Death());
+                StartCoroutine(takeDamageEffect());
             }
 
             if (Input.GetAxis("Horizontal") != 0)
@@ -115,37 +107,6 @@ public class playersControler : MonoBehaviour
             {
                 _renderer.flipX = !(Input.GetAxis("Horizontal") > 0.0f);
             }
-            //----------------------------------------------------------------------------------------------------
-            /*
-            _moviment = new Vector2(Input.GetAxisRaw("Horizontal") * speedForce, 0.0f);
-
-            if (_moviment.sqrMagnitude > 0.1f)
-            {
-                _renderer.flipX = !(Input.GetAxis("Horizontal") > 0.0f);
-            }
-
-            if (Input.GetButtonDown("Jump") && taNoChao == true)
-            {
-                _body.velocity = Vector2.up * jumpForce;
-            }
-
-            if (Input.GetButtonDown("Jump") && taNoChao == false && pulosExtras > 0)
-            {
-                _body.velocity = Vector2.up * jumpForce;
-                pulosExtras--;
-            }
-            if (taNoChao)
-            {
-                pulosExtras = 1;
-            }
-
-            if (taNaAgua)
-            {
-                var posicao_inicial = new Vector3(-20.117f, -1.264f, 0);
-                _body.transform.position = posicao_inicial;
-                Debug.Log("Morreu");
-            }
-            */
         }
     }
 
@@ -153,11 +114,9 @@ public class playersControler : MonoBehaviour
     {
         if (collider.CompareTag("Inimigo"))
         {
-            //var posicao_inicial = new Vector3(-20.117f, -1.264f, 0);
-            //_body.transform.position = posicao_inicial;
             Debug.Log("Morreu");
             StartCoroutine(Death());
-            //StartCoroutine(takeDamageEffect());
+            StartCoroutine(takeDamageEffect());
         }
     }
 
@@ -167,30 +126,38 @@ public class playersControler : MonoBehaviour
         _body.velocity = new Vector2(0, 0);
         yield return new WaitForSeconds(1.0f);
         Application.LoadLevel(Application.loadedLevel);
+        
     }
 
-    //public float effectTime = 0.3f;
-    //IEnumerator takeDamageEffect()
-    //{
-    //    float deltaTime = 0;
-    //    whilhe(deltaTime <= effectTime)
-    //    {
-    //        deltaTime += Time.deltaTime;
-    //        _renderer.color = Color.lerp(Color.white, Color.black, deltaTime / effectTime);
-    //        yield return null;
-    //    }
+    //logica para animação de morte
+    public float effectTime = 0.1f;
+    IEnumerator takeDamageEffect()
+    {
+        pisca = 0;
+        while (pisca <= pisca2)
+        {
+            pisca++;
+            float deltaTime = 0;
+            while (deltaTime <= effectTime)
+            {
+                deltaTime += Time.deltaTime;
+                _renderer.color = Color.Lerp(Color.white, Color.black, deltaTime / effectTime);
+                yield return null;
+            }
 
-    //    yield return new WaitForSeconds(0.1f);
-    //    deltaTime = 0;
-	   // whilhe(deltaTime <= effectTime)
-    //    {
-    //        deltaTime += Time.deltaTime;
-    //        _renderer.color = Color.lerp(Color.black, Color.white, deltaTime / effectTime);
-    //        yield return null;
-    //    }
-    //}
+            yield return new WaitForSeconds(0.1f);
+            deltaTime = 0;
+            while (deltaTime <= effectTime)
+            {
+                deltaTime += Time.deltaTime;
+                _renderer.color = Color.Lerp(Color.black, Color.white, deltaTime / effectTime);
+                yield return null;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
-private void FixedUpdate()
+    private void FixedUpdate()
     {
         taNoChao = Physics2D.OverlapCircle(this.transform.position + offset, raidios, oQueEhChao);
 
