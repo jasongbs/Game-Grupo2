71b0369d2cbf4c4035f336cec05b9daaa30279df
jasongbs <jasongbds@gmail.com>
2021-06-11 19:42:48 -0300

    Final

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -24,7 +24,7 @@ public class camera : MonoBehaviour
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + deltaY, ref velocity.y, smoothTimeY);
      
         float yPos = Mathf.Clamp(posY, -1.9f, 40.0f);
-        float xPos = Mathf.Clamp(posX, -5.0f, 68.0f);
+        float xPos = Mathf.Clamp(posX, -8.0f, 68.0f);
         transform.position = new Vector3(xPos, yPos, transform.position.z);
 
     }
